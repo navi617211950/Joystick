b7bffd7e1aa98492d6c6c93c7d423d3ffac1e1ea
Navis <617211950@qq.com>
2025-10-29 17:04:40 +0800

    6.1.3 函数模板实例

--- doc/noteplus.01.md
+++ doc/noteplus.01.md
@@ -99,6 +99,109 @@ Joystick end.
 * 自动类型推导必须推导出一致的数据类型T才可以使用
 * 模板必须要确定出T的数据类型才可以使用
 
+#### 1.2.3 函数模板实例(数组排序并打印)
+
+代码
+
+```
+#include <iostream>
+
+using namespace std;
+
+/// @brief 函数模板测试命名空间
+namespace methodtemplate
+{
+    /// @brief 声明一个参数交换的函数模板
+    /// @tparam T 模板类
+    /// @param t1 参数1
+    /// @param t2 参数2
+    /// @param u 另一种类型参数
+    template <typename T, typename U>
+    void swapParam(T &t1, T &t2, const U &u)
+    {
+        T temp = t1;
+        t1 = t2;
+        t2 = temp;
+        cout << u << endl;
+    }
+
+    /// @brief 声明一个参数交换的函数模板
+    /// @tparam T 模板类
+    /// @param t1 参数1
+    /// @param t2 参数2
+    template<class T>
+    void swapParam(T &t1, T &t2)
+    {
+        T temp = t1;
+        t1 = t2;
+        t2 = temp;
+    }
+
+
+    /// @brief 排序函数模板
+    /// @tparam T 模板参数
+    /// @param arr 排序数组
+    /// @param len 数组长度
+    template<class T>
+    void sortArray(T arr[], int len)
+    {
+        for(int i = 0; i< len; i++)
+        {
+            int max = i;
+            for(int j=i+1;j < len; j++)
+            {
+                if(arr[max] < arr[j])
+                {
+                    max = j;
+                }
+            }
+            if(max != i)
+            {
+                swapParam(arr[max], arr[i]);
+            }
+        }
+    }
+
+    /// @brief 打印数组
+    /// @tparam T 模板参数类型
+    /// @param arr 数组
+    /// @param len 数组长度
+    template<class T>
+    void print(T arr, int len)
+    {
+        for(int i = 0; i< len; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+
+    void test()
+    {
+        char char_arr[] = "Hello World";
+        int len = sizeof(char_arr) / sizeof(char);
+        sortArray(char_arr, len);
+        print(char_arr, len);
+
+        int int_arr[] = {25,65,2,55,66,982,14,0};
+        len = sizeof(int_arr) / sizeof(int);
+        sortArray(int_arr, len);
+        print(int_arr, len);
+    }
+
+} // namespace methodtemplate
+```
+
+output
+
+```
+Joystick started.
+r o o l l l e d W H
+982 66 65 55 25 14 2 0
+Joystick end.
+```
+
 ### 1.3 类模板
 
 
--- include/testtemplate.h
+++ include/testtemplate.h
@@ -5,17 +5,11 @@ using namespace std;
 /// @brief 函数模板测试命名空间
 namespace methodtemplate
 {
-    void swapInteger(int &a, int &b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
     /// @brief 声明一个参数交换的函数模板
     /// @tparam T 模板类
     /// @param t1 参数1
     /// @param t2 参数2
+    /// @param u 另一种类型参数
     template <typename T, typename U>
     void swapParam(T &t1, T &t2, const U &u)
     {
@@ -25,24 +19,69 @@ namespace methodtemplate
         cout << u << endl;
     }
 
+    /// @brief 声明一个参数交换的函数模板
+    /// @tparam T 模板类
+    /// @param t1 参数1
+    /// @param t2 参数2
+    template<class T>
+    void swapParam(T &t1, T &t2)
+    {
+        T temp = t1;
+        t1 = t2;
+        t2 = temp;
+    }
+
+
+    /// @brief 排序函数模板
+    /// @tparam T 模板参数
+    /// @param arr 排序数组
+    /// @param len 数组长度
+    template<class T>
+    void sortArray(T arr[], int len)
+    {
+        for(int i = 0; i< len; i++)
+        {
+            int max = i;
+            for(int j=i+1;j < len; j++)
+            {
+                if(arr[max] < arr[j])
+                {
+                    max = j;
+                }
+            }
+            if(max != i)
+            {
+                swapParam(arr[max], arr[i]);
+            }
+        }
+    }
+
+    /// @brief 打印数组
+    /// @tparam T 模板参数类型
+    /// @param arr 数组
+    /// @param len 数组长度
+    template<class T>
+    void print(T arr, int len)
+    {
+        for(int i = 0; i< len; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+
     void test()
     {
-        int a = 3;
-        int b = 5;
-        // 两种使用函数模板的方式
-        // 1. 类型自动推导
-        swapParam(a, b, "int");
-        cout << "a = " << a << ", b = " << b << endl;
-        double c = 4.5;
-        double f = 3.4;
-        // 2. 显式指定类型
-        swapParam<double, const char *>(c, f, "double");
-        cout << "c = " << c << ", f = " << f << endl;
+        char char_arr[] = "Hello World";
+        int len = sizeof(char_arr) / sizeof(char);
+        sortArray(char_arr, len);
+        print(char_arr, len);
 
-        string s1 = " World!";
-        string s2 = "Hello";
-        swapParam(s1, s2, "string");
-        cout << s1 << s2 << endl;
+        int int_arr[] = {25,65,2,55,66,982,14,0};
+        len = sizeof(int_arr) / sizeof(int);
+        sortArray(int_arr, len);
+        print(int_arr, len);
     }
 
 } // namespace methodtemplate
